725d5b674106988d54b879149d6781a8cb32c57f
tinobboy <luksbreaking@gmail.com>
2024-09-07 02:39:10 -0300

    Modifico Test

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -1,40 +1,48 @@
-import { test, Browser, Page, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { LoginPageSteps } from './pageSteps/loginPage';
 import { InventoryPageSteps } from './pageSteps/inventoryPage';
 import { ProductsPage } from './pageSteps/productsPage';
 import { CheckoutPage } from './pageSteps/checkoutPage';
-
 import Esperas from "../hooks/esperas";
 
-test('Login and Checkout Test', async ({ page }) => {
-  const esperas = new Esperas(page);
-  const loginPage = new LoginPageSteps(page);
-  const productPage = new ProductsPage(page);
-  const checkoutPage = new CheckoutPage(page);
-  
-  await page.goto('https://www.saucedemo.com');
+test.describe('Ingresar a la Web y Agregar Producto', () => {
 
-  await loginPage.validarTitulo();
-  await loginPage.completarUsuario('myUsername');
-  await loginPage.completarPassword('myPassword');
-  await loginPage.validarBotonLogin();
-  await loginPage.clickIngresar();
+  let esperas: Esperas;
+  let loginPage: LoginPageSteps;
+  let productPage: ProductsPage;
+  let checkoutPage: CheckoutPage;
+  let inventoryPage: InventoryPageSteps;
 
-  const inventoryPage = new InventoryPageSteps(page);
-  await esperas.esperarSegundos(6)
-  await inventoryPage.validarUrl();
-  await inventoryPage.validarTitulo();
-  //await inventoryPage.clickEnBotonOpenMenu();
+  test.beforeEach(async ({ page }) => {
+    esperas = new Esperas(page);
+    loginPage = new LoginPageSteps(page);
+    productPage = new ProductsPage(page);
+    checkoutPage = new CheckoutPage(page);
+    inventoryPage = new InventoryPageSteps(page);
 
-  await productPage.addProductToCart();
-  await productPage.goToCart();
+    await page.goto('https://www.saucedemo.com');
+    await loginPage.validarTitulo();
+    await loginPage.completarUsuario('myUsername');
+    await loginPage.completarPassword('myPassword');
+    await loginPage.validarBotonLogin();
+    await loginPage.clickIngresar();
+  });
 
-  await checkoutPage.checkOut();
-  await checkoutPage.fillInformationForm();
-  await checkoutPage.finishCheckOut();
-  await checkoutPage.validateSuccessMessage();
+  test('Agregar Producto y Hacer Checkout Del Producto', async ({ page }) => {
+    await esperas.esperarSegundos(6);
+    await inventoryPage.validarUrl();
+    await inventoryPage.validarTitulo();
+    //await inventoryPage.clickEnBotonOpenMenu();
+    await productPage.addProductToCart();
+    await productPage.goToCart();
+    await checkoutPage.checkOut();
+    await checkoutPage.fillInformationForm();
+    await checkoutPage.finishCheckOut();
+    await checkoutPage.validateSuccessMessage();
+  });
 
-  await page.close();
-      
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
+
 });
-
